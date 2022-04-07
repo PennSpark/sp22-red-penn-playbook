c15a1f2702674b88700965b63be96b11813b5ba7
Anna Xia <44536369+annaanx24@users.noreply.github.com>
2022-04-07 00:11:49 -0400

    Add under the button homepage and question routes, unstyled

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 //components
 import Form from './components/Form'
 import Home from './components/Home'
+import UnderButton from './components/UnderButton'
 
 export function App() {
     return (
@@ -13,6 +14,10 @@ export function App() {
             <Route path='/'>
               <Route index element={<Navigate replace to='Home'/>} />
               <Route path='Home' element={<Home />} />
+            </Route>
+            <Route path='/under-the-button'>
+              <Route path=":qId" element={<UnderButton />} />
+              <Route index element={<UnderButton />} />
             </Route>
           </Routes>
         </BrowserRouter>

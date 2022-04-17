28f90c5bed878c259e1173d3a0ee1988a24dbdd2
exyzhao <ethanxyzhao@gmail.com>
2022-04-17 00:40:59 -0400

    Stylize under the button login

--- src/components/UnderButton.js
+++ src/components/UnderButton.js
@@ -18,13 +18,22 @@ const UnderButton = () => {
     }
     return (
         <>
-        <Navbar/>
-            {!qId && <h1>Under the Button</h1>}
-            {!qId && <h4>Under the Button is a quiz to see how average or ~ unique ~ of a Penn Student you are!
-                Be ready to express your opinions on some of the most controversal questions like milk or cereal first!</h4>}
-
-            {!qId && <h4>Penn Email</h4>}
-            {!qId && <input onChange={e => setEmail(e.target.value)} type="name" className="form-control" id="name" />}
+            <Navbar />
+            <div class="utb-description">
+                {!qId && <h1>Under the Button</h1>}
+                {!qId && <p>Under the Button is a quiz to see how average or ~ unique ~ of a Penn Student you are!
+                    Be ready to express your opinions on some of the most controversal questions like milk or cereal first!</p>}
+            </div>
+            <div class="login-container">
+                <div class="login">
+                    {!qId && <h2>Log in</h2>}
+                    {!qId && <img src="svg/button-outline.svg" alt="Button" />}
+                    <div class="login-inputs">
+                        {!qId && <h4>Penn Email</h4>}
+                        {!qId && <input onChange={e => setEmail(e.target.value)} type="name" className="form-control" id="name" />}
+                    </div>
+                </div>
+            </div>
             {!qId && <br />}
             {!qId && email && <Link to="../1">
                 <button type="button" onClick={() => signUp()}>

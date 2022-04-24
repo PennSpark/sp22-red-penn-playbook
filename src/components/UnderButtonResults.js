ef071c1ec710b1dfe9ce081a15107e7a64552bfc
exyzhao <ethanxyzhao@gmail.com>
2022-04-24 02:20:06 -0400

    Stylize utb results page

--- src/components/UnderButtonResults.js
+++ src/components/UnderButtonResults.js
@@ -3,6 +3,7 @@ import {
 } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './UnderButtonResults.css';
 
 const UnderButtonResults = () => {
   const [typeOfStudent, setTypeOfStudent] = useState('')
@@ -35,8 +36,8 @@ const UnderButtonResults = () => {
     getResults()
 }, [min])
   return (
-    <>
-        <h1 className="results-h1">Here are your results!</h1>
+    <div>
+        <h1 className="results-h1">By the Button</h1>
         { rndInt === 1 && answers[3] == "milk first" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who prefers crispy cereal and would rather have a {answers[7]} in your life</h2>}
         { rndInt === 1 && answers[3] == "cereal first" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who prefers soggy cereal and would rather have a {answers[7]} in your life</h2>}
         { rndInt === 1 && answers[3] == "no cereal" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who doesn't like cereal and would rather have a {answers[7]} in your life</h2>}
@@ -46,7 +47,7 @@ const UnderButtonResults = () => {
         { rndInt === 3 && answers[14] == "oversleep every day for a week" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who probably shows up late to every class from oversleeping </h2> }
         { rndInt === 3 && answers[14] == "no sleep for 4 days" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who probably pulls way too many all-nighters </h2> }
         <h3 className="results-h3">You chose the minority answer in {min}/15 of the questions</h3>
-    </>
+    </div>
   )
 }
 

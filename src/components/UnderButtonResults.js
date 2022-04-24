c0dd3f9bbe08a0d8cf38e607e8a99cf345cc1311
aJayz54 <andrewjiang311@gmail.com>
2022-04-24 02:03:07 -0400

    utb-stuff

--- src/components/UnderButtonResults.js
+++ src/components/UnderButtonResults.js
@@ -36,11 +36,16 @@ const UnderButtonResults = () => {
 }, [min])
   return (
     <>
-        <h1>Here are your results!</h1>
-        { rndInt === 1 && <h2>You are a {typeOfStudent} Penn student as you believe the right way to eat cereal is {answers[3]} and would rather have a {answers[7]} button in your life</h2>}
-        { rndInt === 2 && <h2>You are a {typeOfStudent} Penn student as you would rather {answers[5]} and {answers[12]} </h2> }
-        { rndInt === 3 && <h2>You are a {typeOfStudent} Penn student as your toilet paper preference is {answers[13]} and would rather know {answers[6]} </h2> }
-        <h3>You chose the minority answer in {min}/15 of the questions</h3>
+        <h1 className="results-h1">Here are your results!</h1>
+        { rndInt === 1 && answers[3] == "milk first" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who prefers crispy cereal and would rather have a {answers[7]} in your life</h2>}
+        { rndInt === 1 && answers[3] == "cereal first" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who prefers soggy cereal and would rather have a {answers[7]} in your life</h2>}
+        { rndInt === 1 && answers[3] == "no cereal" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who doesn't like cereal and would rather have a {answers[7]} in your life</h2>}
+        { rndInt === 1 && answers[3] == "~ warm ~ cereal" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who prefers warm cereal and would rather have a {answers[7]} in your life</h2>}
+        { rndInt === 2 && answers[5] == "change genders every time you sneeze" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who would have an identity crisis during allergy season</h2> }
+        { rndInt === 2 && answers[5] == "not be able to tell the difference between babies and muffins" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who probably wants to eat babies </h2> }
+        { rndInt === 3 && answers[14] == "oversleep every day for a week" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who probably shows up late to every class from oversleeping </h2> }
+        { rndInt === 3 && answers[14] == "no sleep for 4 days" && <h2 className="results-h2">You are a {typeOfStudent} Penn student who probably pulls way too many all-nighters </h2> }
+        <h3 className="results-h3">You chose the minority answer in {min}/15 of the questions</h3>
     </>
   )
 }

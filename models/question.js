d298250395af4dc69ed9bb791d330c88e493b4f0
Christina Lu <christina.lu.12@gmail.com>
2022-04-25 23:04:51 -0400

    some magic

--- models/question.js
+++ models/question.js
@@ -1,17 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const { Schema, model } = mongoose
-
-const questionSchema = new Schema({
+const questionSchema = new mongoose.Schema({
   number: { type: Number, required: true },
-  text: { type: String, required: true},
-  op1: { type: String},
-  op2: { type: String},
-  op3: { type: String},
-  op4: { type: String},
-  numOp: { type: Number, required: true }
-})
+  text: { type: String, required: true },
+  op1: { type: String },
+  op2: { type: String },
+  op3: { type: String },
+  op4: { type: String },
+  numOp: { type: Number, required: true },
+});
 
-const Question = model('Question', questionSchema)
+const Question = mongoose.model("Question", questionSchema);
 
-module.exports = Question
+module.exports = Question;

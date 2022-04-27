f5417a661a8ef585e0dc7f8021ef9301aa5d31de
exyzhao <ethanxyzhao@gmail.com>
2022-04-27 00:25:34 -0400

    Create looking back page for PW

---
+++ client/src/components/penn-wrapped/LookingBack.js
@@ -0,0 +1,15 @@
+import React, { useContext, useState } from 'react'
+import { useParams, Link } from "react-router-dom"
+import './PennWrapped.css'
+
+const LookingBack = () => {
+    return (
+        <div>
+            <div class="lb-container">
+            <p>hi</p>
+            </div>
+        </div>
+    );
+}
+
+export default LookingBack;

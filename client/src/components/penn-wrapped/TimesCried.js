f19dfc96421ba0f7ed16b67c7728f0448db776e2
exyzhao <ethanxyzhao@gmail.com>
2022-04-27 03:20:46 -0400

    Create js and scss files for 4,5,6 in PW

--- client/src/components/penn-wrapped/TimesCried.js
+++ client/src/components/penn-wrapped/TimesCried.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { useParams, Link } from "react-router-dom"
-import './FratParties.scss';
+import './TimesCried.scss';
 import Frat0 from '../svg/frat0.svg';
 import Frat3 from '../svg/frat3.svg';
 import Frat6 from '../svg/frat6.svg';
@@ -8,7 +8,7 @@ import Frat6 from '../svg/frat6.svg';
 const TimesCried = () => {
     return (
         <div>
-            <div class="fp-container">
+            <div class="tc-container">
             <h1>"How many frat parties did you go to?"</h1>
                 <div class="fp-row">
                     <img src={Frat0} alt="28.1% have 1-3 close friends" />

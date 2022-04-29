81f733acde2c6597d18954654d46df7b5a14cea6
exyzhao <ethanxyzhao@gmail.com>
2022-04-29 11:24:34 -0400

    Move text in rp and three words

--- client/src/components/penn-wrapped/RicePurity.js
+++ client/src/components/penn-wrapped/RicePurity.js
@@ -7,14 +7,12 @@ import rice from "./img/rice.gif"
 const RicePurity = () => {
   return (
     <div className="pw-container rp-block">
-      <div className="spacer-10" />
+      <div className="rp-left" />
       <div id="rp-text">
           <h1>On average, our rice purity score dropped <span style={{color: "#577ED2"}}>5.9 points</span>.</h1>
-          <p>For these reasons...</p>
       </div>
-      <div className="spacer-3" />
+      <div className="rp-right" />
         <img id="rp-img" src={rice} />
-      <div className="spacer-10" />
     </div>
   );
 };

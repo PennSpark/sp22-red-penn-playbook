16c5b61460ad15905753592cd9afc34e4447541a
olivia zha <olz@sas.upenn.edu>
2022-04-29 02:33:35 -0400

    fixed arrow

--- client/src/components/PennWrappedAbout.js
+++ client/src/components/PennWrappedAbout.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./PennWrappedAbout.scss";
-import RightArrow from "./svg/rightArrow.svg";
+import { ReactComponent as RightArrow } from "./svg/rightArrow.svg";
 
 
 const PennWrappedAbout = () => {
